8590ec5c12b94859336490b90e68ac2591b894e3
ConsonantSpork <dimabelov.tu99@gmail.com>
2021-04-09 09:56:39 +0300

    Add setup.py to ship python module

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from cmake_build_extension import BuildExtension, CMakeExtension
+
+from os import path
+
+setup(
+    name='pyduk',
+    version='0.1',
+    description='Duktape engine wrapper for Python 3',
+    author="Dmitry Belov",
+    author_email="dimabelov.tu99@gmail.com",
+    packages=find_packages('python'),
+    package_dir={'': 'python'},
+    ext_modules=[
+        CMakeExtension(name='_pyduk',
+                       install_prefix='pyduk',
+                       cmake_configure_options=[
+                           '-DMODULE_ONLY=ON',
+                       ]),
+    ],
+    cmdclass={'build_ext' : BuildExtension},
+    zip_safe=False
+)
