eb23e36425ac216f59c5f2b3660d656f4236260e
ConsonantSpork <dimabelov.tu99@gmail.com>
2021-05-25 14:44:34 +0300

    Restructure tests

--- test/circular_references.cpp
+++ test/circular_references.cpp
@@ -2,18 +2,20 @@
 #include "context_fixture.hpp"
 #include <boost/python.hpp>
 
-TEST_F(PyDukContext, list_circular_reference) {
+class CircularReferences : public PyDukContext {};
+
+TEST_F(CircularReferences, list_circular_reference) {
     boost::python::object obj = context->run("var l = []; l.push(l); l;");
     boost::python::list list(obj);
     EXPECT_EQ(list, list[0]);
 }
 
-TEST_F(PyDukContext, object_circular_reference) {
+TEST_F(CircularReferences, object_circular_reference) {
     boost::python::object obj = context->run("var l = {}; l.circ = l; l;");
     EXPECT_EQ(obj, obj["circ"]);
 }
 
-TEST_F(PyDukContext, complex_circular_reference) {
+TEST_F(CircularReferences, complex_circular_reference) {
     boost::python::object obj = context->run("var l = {key1: [1, 2], key2: 'hello'}; l.key1.push(l); l");
     EXPECT_EQ(obj["key2"], "hello");
     EXPECT_EQ(obj["key1"][0], 1);
@@ -21,7 +23,7 @@ TEST_F(PyDukContext, complex_circular_reference) {
     EXPECT_EQ(obj["key1"][2], obj);
 }
 
-TEST_F(PyDukContext, multiple_circular_references) {
+TEST_F(CircularReferences, multiple_circular_references) {
     boost::python::object obj = context->run("var o = {}; var l = []; o.key = o; l.push(l); o.list = l; o;");
     EXPECT_EQ(obj["key"], obj);
     EXPECT_EQ(obj["list"], obj["list"][0]);
--- test/conversion.cpp
+++ test/conversion.cpp
@@ -2,12 +2,14 @@
 #include "context_fixture.hpp"
 #include <boost/python.hpp>
 
-TEST_F(PyDukContext, string_conversion) {
+class Conversion : public PyDukContext {};
+
+TEST_F(Conversion, string_conversion) {
     EXPECT_EQ(context->run("'Hello, World!'"),
               boost::python::object("Hello, World!"));
 }
 
-TEST_F(PyDukContext, integer_conversion) {
+TEST_F(Conversion, integer_conversion) {
     auto actual = context->run("1 + 2");
     EXPECT_EQ(PyLong_Check(actual.ptr()),
               true);
@@ -15,34 +17,34 @@ TEST_F(PyDukContext, integer_conversion) {
               boost::python::object(3));
 }
 
-TEST_F(PyDukContext, number_conversion) {
+TEST_F(Conversion, number_conversion) {
     EXPECT_EQ(context->run("1.2 + 2.3"),
               boost::python::object(3.5));
 }
 
-TEST_F(PyDukContext, boolean_conversion) {
+TEST_F(Conversion, boolean_conversion) {
     EXPECT_EQ(context->run("true"),
               boost::python::object(true));
 }
 
-TEST_F(PyDukContext, boolean_object_conversion) {
+TEST_F(Conversion, boolean_object_conversion) {
     EXPECT_EQ(context->run("new Boolean(1)"),
               boost::python::object(true));
 }
 
-TEST_F(PyDukContext, empty_object_conversion) {
+TEST_F(Conversion, empty_object_conversion) {
     boost::python::dict expected;
     EXPECT_EQ(context->run("var d = {}; d;"),
               expected);
 }
 
-TEST_F(PyDukContext, empty_list_conversion) {
+TEST_F(Conversion, empty_list_conversion) {
     boost::python::list expected;
     EXPECT_EQ(context->run("var d = []; d;"),
               expected);
 }
 
-TEST_F(PyDukContext, list_conversion) {
+TEST_F(Conversion, list_conversion) {
     boost::python::list expected;
     boost::python::list inner;
     inner.append("inner");
@@ -55,62 +57,62 @@ TEST_F(PyDukContext, list_conversion) {
               expected);
 }
 
-TEST_F(PyDukContext, number_object) {
+TEST_F(Conversion, number_object) {
     EXPECT_EQ(context->run("new Number(12.0)"),
               boost::python::object(12));
 }
 
-TEST_F(PyDukContext, number_object_float) {
+TEST_F(Conversion, number_object_float) {
     EXPECT_EQ(context->run("new Number(12.4)"),
               boost::python::object(12.4));
 }
 
-TEST_F(PyDukContext, array_buffer_conversion) {
+TEST_F(Conversion, array_buffer_conversion) {
     EXPECT_EQ(context->run("new Int8Array([258, 2, 0, -2]).buffer"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x02\x02\x00\xfe", 4)));
 }
 
-TEST_F(PyDukContext, uint8_array_conversion) {
+TEST_F(Conversion, uint8_array_conversion) {
     EXPECT_EQ(context->run("new Uint8Array([258, 2, 0, -2]).buffer"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x02\x02\x00\xfe", 4)));
 }
 
-TEST_F(PyDukContext, int8_array_conversion) {
+TEST_F(Conversion, int8_array_conversion) {
     EXPECT_EQ(context->run("new Int8Array([130, -2, 3, 256, 0, 1])"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x82\xfe\x03\x00\x00\x01", 6)));
 }
 
-TEST_F(PyDukContext, uint16_array_conversion) {
+TEST_F(Conversion, uint16_array_conversion) {
     EXPECT_EQ(context->run("new Uint16Array([256, -1])"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x00\x01\xff\xff", 4)));
 }
 
-TEST_F(PyDukContext, int16_array_conversion) {
+TEST_F(Conversion, int16_array_conversion) {
     EXPECT_EQ(context->run("new Int16Array([256, -1])"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x00\x01\xff\xff", 4)));
 }
 
-TEST_F(PyDukContext, uint32_array_conversion) {
+TEST_F(Conversion, uint32_array_conversion) {
     EXPECT_EQ(context->run("new Uint32Array([65536, -1])"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x00\x00\x01\x00\xff\xff\xff\xff", 8)));
 }
 
-TEST_F(PyDukContext, int32_array_conversion) {
+TEST_F(Conversion, int32_array_conversion) {
     EXPECT_EQ(context->run("new Int32Array([65536, -1])"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x00\x00\x01\x00\xff\xff\xff\xff", 8)));
 }
 
 
-TEST_F(PyDukContext, float32_array_conversion) {
+TEST_F(Conversion, float32_array_conversion) {
     EXPECT_EQ(context->run("new Float32Array([12.3])"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\xcd\xcc\x44\x41", 4)));
 }
 
-TEST_F(PyDukContext, float64_array_conversion) {
+TEST_F(Conversion, float64_array_conversion) {
     EXPECT_EQ(context->run("new Float64Array([12.3])"),
               boost::python::handle<>(PyByteArray_FromStringAndSize("\x9a\x99\x99\x99\x99\x99\x28\x40", 8)));
 }
-TEST_F(PyDukContext, simple_object_conversion) {
+TEST_F(Conversion, simple_object_conversion) {
     boost::python::dict expected;
     expected["hello"] = "world";
     expected["key"] = 2;
@@ -118,7 +120,7 @@ TEST_F(PyDukContext, simple_object_conversion) {
               expected);
 }
 
-TEST_F(PyDukContext, complex_object_conversion) {
+TEST_F(Conversion, complex_object_conversion) {
     boost::python::dict expected;
     boost::python::dict inner;
     boost::python::list list;
